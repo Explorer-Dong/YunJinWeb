fe2d8989236252c7432e1380e6c0ddb7721f6939
Mr_Dwj <explorer-dong@foxmail.com>
2023-12-07 20:29:12 +0800

    第一次部署上云

--- app.py
+++ app.py
@@ -1,71 +1,79 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+import config
+from exts import db
+from models import Poem
 
 app = Flask(__name__)
+app.config.from_object(config)
+db.init_app(app)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@localhost:3306/poem"
-
-
-db = SQLAlchemy(app)
-
-
-class Poem(db.Model):
-	__tablename__ = "poem_table"
-	dynasty = db.Column(db.String(10), primary_key=True)
-	ori = db.Column(db.String(100))
-	fro = db.Column(db.String(30))
-	kind = db.Column(db.String(10))
-	
-	def __init__(self, dynasty, ori, fro, kind):
-		self.dynasty = dynasty
-		self.ori = ori
-		self.fro = fro
-		self.kind = kind
+# 将ORM模型与数据库进行同步
+# migrate = Migrate(app, db)
 
 
 @app.route("/", methods=["POST", "GET"])
 def homepage():
 	"""
-	参数列表：
+	输入：
+		1. 搜索字段复选框: search_fields
+		2. 搜索关键词表单: search_keyword
+	返回：
+		1. keyword: 搜索关键词
+		2. poems: 过滤出来的诗词信息
+		3. fields: 搜索字段
 	"""
 	
 	if request.method == "POST":
-		# 搜索状态
-		keyword = request.form.get("search_of_key", "")
-		poems = Poem.query.filter(Poem.ori.like("%" + keyword + "%"))
+		# 输入：搜索字段 & 搜索关键词
+		fields = request.form.getlist("search_fields")
+		keyword = request.form.get("search_keyword")
 		
-		for it in poems:
-			print(it)
-
-		if poems.first() is None:
+		if not len(fields):
+			# 无搜索字段，默认搜索诗词原文
+			fields.append("ori")
+			poems = Poem.query.filter(Poem.ori.like("%" + keyword + "%"))
+		else:
+			# 有搜索字段，按照字段依次搜索
+			filters = []
+			for field in fields:
+				if field == "dynasty":
+					filters.append(Poem.dynasty.like("%" + keyword + "%"))
+				elif field == "ori":
+					filters.append(Poem.ori.like("%" + keyword + "%"))
+				elif field == "fro":
+					filters.append(Poem.fro.like("%" + keyword + "%"))
+				elif field == "kind":
+					filters.append(Poem.kind.like("%" + keyword + "%"))
+			poems = Poem.query.filter(*filters)
+		
+		if not poems.count():
+			# 无搜索结果：提示用户重新输入
 			return render_template("homepage.html", poems=[], keyword="__error__")
 		else:
-			return render_template("homepage.html", poems=poems, keyword=keyword)
-		
-	else: # request.method == "GET"
-		# 初始进入当前界面状态
+			# 有搜索结果：返回搜索结果
+			return render_template("homepage.html", poems=poems, keyword=keyword, fields=fields)
+	
+	else:
+		# 初始界面，提示用户输入
 		return render_template("homepage.html", poems=[], keyword="__new__")
 
 
-@app.route("/add1", methods=["POST", "GET"])
-def add1():
-	return render_template("add1.html")
+@app.route("/files", methods=["POST", "GET"])
+def files():
+	return render_template("files.html")
 
 
-@app.route("/add2", methods=["POST", "GET"])
-def add2():
-	return render_template("add2.html")
+@app.route("/intro", methods=["POST", "GET"])
+def intro():
+	return render_template("intro.html")
 
 
-@app.route("/add3", methods=["POST", "GET"])
-def add3():
-	return render_template("add3.html")
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html'), 404
+@app.route("/photo", methods=["POST", "GET"])
+def photo():
+	return render_template("photo.html")
 
 
 if __name__ == "__main__":
---
+++ models.py
@@ -0,0 +1,25 @@
+# @Time   : 2023-12-03 23:33
+# @File   : models.py
+# @Author : Mr_Dwj
+
+'''
+ORM映射模型
+	1. Poem: 诗词表
+	2. ...
+'''
+
+from exts import db
+
+
+class Poem(db.Model):
+	__tablename__ = "poem_table"
+	dynasty = db.Column(db.String(10), primary_key=True)
+	ori = db.Column(db.String(100))
+	fro = db.Column(db.String(30))
+	kind = db.Column(db.String(10))
+	
+	def __init__(self, dynasty, ori, fro, kind):
+		self.dynasty = dynasty  # 朝代
+		self.ori = ori          # 原文
+		self.fro = fro          # 朝代
+		self.kind = kind        # 文体
